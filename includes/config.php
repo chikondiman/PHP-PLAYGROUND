5313581b9722165dac7a5a553b36df5085c6cc90
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-10-16 19:53:02 -0400

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -19,7 +19,7 @@ function config($key = '')
         ],
         'template_path' => 'template',
         'content_path' => 'content',
-        'version' => 'v3.1',
+        'version' => 'v20.20',
     ];
 
     return isset($config[$key]) ? $config[$key] : null;
