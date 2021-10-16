cf180d038eb894a08c197e446a5629a1c3352d04
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-10-16 19:52:25 -0400

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Hot Shot',
+        'name' => 'Hot Shot$',
         'site_url' => '',
         'pretty_uri' => false,
         'nav_menu' => [
