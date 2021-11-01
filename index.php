a1a196d8798e0a685e8b1822fa7114ba5c1dbefe
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-11-01 12:50:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
 <html>
-<body>
-<?php $a = 10; // global scope
-function myTest() {
-// using a inside this function will generate an error
-echo "<p>Variable a inside function is: $a</p>";
-}
-myTest();
-echo "<p>Variable a outside function is: $a</p>";
-? >
-</html>
+	<body>
+	<?PHP
+		echo "<h1>studentstutorial.com</h1>";
+		echo "welcome to PHP world <br>";
+	?>
+	</body>
+	</html>
