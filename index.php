fe6fb6f82d80bfe8f50f9ff89b4f309e821e84a0
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-11-01 12:54:08 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
 <html>
-<body>
-<?PHP
-$a = 20;
-$b= 10;
-echo $a + $b;
-
-? >
-
-</body>
-</html>
+	<body>
+	<?PHP
+		echo "<h1>studentstutorial.com</h1>";
+		echo "welcome to PHP world <br>";
+	?>
+	</body>
+	</html>
