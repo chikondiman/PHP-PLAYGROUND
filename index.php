f7c15d6f3bcdf221f5242d55d2722f1f1349754b
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-11-04 15:30:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 namespace Codecademy;
 
-$learner_one = ["is_correct"=>FALSE, "box"=>["shape"=>"none", "color"=>"none"]];
-   
-$learner_two = ["is_correct"=>TRUE, "box"=>["shape"=>"none", "color"=>"none"]];
-  
 
 chooseCheckoutLane(){
   if ($custmerItems>12)
+  {
+    return "express lane";
+  }
+  else{
+    "regular lane";
+  }
 }
