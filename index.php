a3a704f815a9289038b926f0fa6c031b781d5851
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-11-04 15:29:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,4 +5,7 @@ $learner_one = ["is_correct"=>FALSE, "box"=>["shape"=>"none", "color"=>"none"]];
    
 $learner_two = ["is_correct"=>TRUE, "box"=>["shape"=>"none", "color"=>"none"]];
   
-  
+
+chooseCheckoutLane(){
+  if ($custmerItems>12)
+}
