1fe66a6da9fdfe690fe2e5032cce7d5f12b30b0d
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-11-05 07:34:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
 namespace Codecademy;
 
-
-function agreeOrDisagree($string1, $string2){
-  if ($string1 === $string2){}
-    return "You Agree";
+function agreeOrDisagree($string1,$string2){
+  if ($string1 === $string2){
+    return "You agree!";
   }
-  else {
-    return "You Disagree";
+  else{
+    return "You disagree!";
   }
 }
