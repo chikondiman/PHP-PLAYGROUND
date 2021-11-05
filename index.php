3cd0125687ee63bf43310ae83a457faacb281d8a
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-11-05 07:45:43 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,3 +12,17 @@ function agreeOrDisagree($string1,$string2){
 
 echo agreeOrDisagree("not","there");
 echo agreeOrDisagree("not","not");
+
+function checkRenewalMonth($renewalMonth){
+  if (date("F") !==     $renewalMonth)
+  {
+  return "Welcome!";
+}
+else {
+  return "Time to renew";
+}
+}
+
+echo checkRenewalMonth("January");
+
+echo checkRenewalMonth("November");
