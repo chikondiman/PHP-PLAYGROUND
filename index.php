410543b8f783053924c7b3c742b30641d5630c63
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-11-07 09:28:29 -0600

    Update index.php

--- index.php
+++ index.php
@@ -31,3 +31,25 @@ if ( $isClicked ) {
 } else {
   $link_color = "blue";
 }
+
+
+<?php
+namespace Codecademy;
+
+function ternaryCheckout($items)
+{
+    return $items <= 12 ? "express lane" : "regular lane";
+}
+
+function ternaryVote ($age) 
+{
+  return $age >= 18 ? "yes" : "no";
+ }
+
+echo ternaryCheckout(1);
+echo "\n\n";
+echo ternaryCheckout(13);
+echo "\n\n";
+echo ternaryVote(19);
+echo "\n\n";
+echo ternaryVote(13);
