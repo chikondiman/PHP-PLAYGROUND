095404dcaef0c57a8f36fb2173bf37eee9eb2b70
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-11-07 09:40:25 -0600

    Update index.php

--- index.php
+++ index.php
@@ -90,3 +90,20 @@ echo truthyOrFalsy("-10290192.871");
 echo "\n\n";
 echo truthyOrFalsy("0");
 echo "\n\n";
+
+<?php
+namespace Codecademy;
+
+echo "Hello, there. What's your first name?\n";  
+
+$name = readline(">> ");
+
+$name_length = strlen($name);
+
+if ($name_length > 8) {
+  echo "Hi, ${name}. That's a long name.";
+} elseif ($name_length > 3) {
+  echo "Hi, ${name}.";
+} else {
+  echo "Hi, ${name}. That's a short name.";
+}
